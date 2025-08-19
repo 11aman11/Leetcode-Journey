bc6b96aec2de7d68212c2720ae74785a15edbdac
aman <112495353+11aman11@users.noreply.github.com>
2025-08-19 12:13:33 -0400

    Create 1004-MaxConsecutiveOnesIII

---
+++ 1004-MaxConsecutiveOnesIII/version/python/1004-MaxConsecutiveOnesIII_20250819_121332.py
@@ -0,0 +1,19 @@
+# Last updated: 8/19/2025, 12:13:32 PM
+# sliding window technique
+class Solution(object):
+    def longestOnes(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        left = curr = ans = 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                curr += 1
+            while curr > k:
+                if nums[left] == 0:
+                    curr -= 1
+                left += 1
+            ans = max(ans, right - left + 1)
+        return ans
