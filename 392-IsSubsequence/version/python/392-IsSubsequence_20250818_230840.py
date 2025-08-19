7e5e62838702c9dd1f468c8d7d8213439dd2dec8
aman <112495353+11aman11@users.noreply.github.com>
2025-08-18 23:08:41 -0400

    Create 392-IsSubsequence

---
+++ 392-IsSubsequence/version/python/392-IsSubsequence_20250818_230840.py
@@ -0,0 +1,25 @@
+# Last updated: 8/18/2025, 11:08:40 PM
+# couldn't see the bug i wrote s[i] = s[j] instead of t[j] damn!!
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i = 0 
+        j = 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+        return i == len(s)
+
+        i = j = 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+        return i == len(s)
