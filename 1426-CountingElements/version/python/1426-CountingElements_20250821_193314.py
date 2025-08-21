de01a63f6b26db52aac0e3bf5c4aa1d2a67df1dc
aman <112495353+11aman11@users.noreply.github.com>
2025-08-21 19:33:15 -0400

    Create 1426-CountingElements

---
+++ 1426-CountingElements/version/python/1426-CountingElements_20250821_193314.py
@@ -0,0 +1,14 @@
+# Last updated: 8/21/2025, 7:33:14 PM
+# mistakes: used hash dict instead of set easier, did count for frequencty instead use counter to add values but before turn the nums in set
+class Solution(object):
+    def countElements(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        nums = set(arr)
+        freq = 0
+        for i in range(len(arr)):
+            if (arr[i] + 1) in nums:
+                freq += 1
+        return freq
