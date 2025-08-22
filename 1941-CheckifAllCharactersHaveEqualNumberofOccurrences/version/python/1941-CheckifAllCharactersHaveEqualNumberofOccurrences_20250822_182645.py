d365d5923bbf357222a9ef81023c2df5c4683769
aman <112495353+11aman11@users.noreply.github.com>
2025-08-22 18:26:45 -0400

    Create 1941-CheckifAllCharactersHaveEqualNumberofOccurrences

---
+++ 1941-CheckifAllCharactersHaveEqualNumberofOccurrences/version/python/1941-CheckifAllCharactersHaveEqualNumberofOccurrences_20250822_182645.py
@@ -0,0 +1,21 @@
+# Last updated: 8/22/2025, 6:26:45 PM
+class Solution(object):
+    def areOccurrencesEqual(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        freq = defaultdict(int)
+        for char in s:
+            freq [char] += 1
+
+        times = freq[s[0]]
+
+        for key in freq:
+            if freq[key] != times:
+                return False
+        return True    
+
+        #smarter way instead of for
+        # times = freq.values()
+        # return len(set(times)) == 1
