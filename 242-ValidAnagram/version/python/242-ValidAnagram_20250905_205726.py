62d9e71ea5de31038b2f86f4a4d296f1a89b6010
aman <112495353+11aman11@users.noreply.github.com>
2025-09-05 20:57:27 -0400

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/version/python/242-ValidAnagram_20250905_205726.py
@@ -0,0 +1,16 @@
+# Last updated: 9/5/2025, 8:57:26 PM
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        mapS, mapT = {}, {}
+        for i in range(len(s)):
+            mapS[s[i]] = mapS.get(s[i], 0) + 1
+            mapT[t[i]] = mapT.get(t[i], 0) + 1
+        return mapS == mapT
+
